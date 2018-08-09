cda4cb6dde037417a58a3eb320e12188351fd7a5
nivred <emaildervin@gmail.com>
2018-08-08 23:06:11 -0500

    bootstrap style changes and HTML refactorying

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -45,12 +45,12 @@ function fetchIssues(){
         var assignedTo = issues[i].assignedTo;
         var status = issues[i].status;
 
-        issuesList.innerHTML += '<div class="well>'+
-                                '<h6>Issue ID: ' + id + '</h6>' +
-                                '<p><span class="label label-info">' + status + '</span></p>' +
+        issuesList.innerHTML += '<div>'+
+                                '<h6 class="text-muted">Issue ID: ' + id + '</h6>' +
+                                '<p><span class="badge badge-dark p-1">' + status + '</span></p>' +
                                 '<h3>' + desc + '</h3>' +
-                                '<p><span class="glyphicon glyphicon-time"></span>' + severity + '</p>' +
-                                '<p><span class="glyphicon glyphicon-time"></span>' + assignedTo + '</p>' +
+                                '<p><i class="fas fa-exclamation-circle mr-2"></i>' + severity + '</p>' +
+                                '<p><i class="fas fa-user mr-2"></i>' + assignedTo + '</p>' +
                                 '<a href="#" onclick="setStatusClosed(\''+id+'\')" class="btn btn-warning mr-1">Close</a>' +
                                 '<a href="#" onclick="deletIssue(\''+id+'\')" class="btn btn-danger">Delete</a>' +
                                 '</div>'
